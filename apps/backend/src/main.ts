efa6c39e806d228220b0ac4c5c0fa273411da91e
Yovie Fesya Pratama <yoviefp@gmail.com>
2025-02-17 14:54:27 +0700

    fix: cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://test-fe-beta.vercel.app', // Allow your frontend
-    credentials: true, // If you're using cookies or authorization headers
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
